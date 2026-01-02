97be4287c7a14a8edaafcec827e812371c4f59fe
bibekthapa1 <bibekthapa00f@gmail.com>
2026-01-02 15:41:51 +0545

    feat: made a simple game using chat hugging face api.

--- chats/chat_hugging_face.py
+++ chats/chat_hugging_face.py
@@ -1,18 +1,35 @@
+from datetime import datetime
+
+now = datetime.now()
+print("Starttime:", now)
+print("=========")
 from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
 from dotenv import load_dotenv
 
 load_dotenv()
 
-llm = HuggingFaceEndpoint(
-    repo_id="deepseek-ai/DeepSeek-R1-0528",
-    task="text-generation",
-    max_new_tokens=200,
-    do_sample=False,
-    repetition_penalty=1.03,
-    provider="auto"
-)
+case = bool(input("Welcome to the game. Press y to continue and enter to discontinue. "))
 
-chat_model = ChatHuggingFace(llm=llm)
+while case:
+    question = input("Ask anything ")
+    llm = HuggingFaceEndpoint(
+        repo_id="deepseek-ai/DeepSeek-R1-0528",
+        task="text-generation",
+        max_new_tokens=200,
+        do_sample=False,
+        repetition_penalty=1.03,
+        provider="auto"
+    )
 
-result = chat_model.invoke("What is the capital city of Nepal?")
-print(result)
+    chat_model = ChatHuggingFace(llm=llm)
+
+    result = chat_model.invoke(question)
+    print(result.content)
+
+    case = bool(input("Type y to continue. Press enter to discontinue. "))
+
+
+print("========")
+end_time = datetime.now()
+print("End Time", end_time)
+print("Total TIme (in seconds): ", (end_time - now).total_seconds())
